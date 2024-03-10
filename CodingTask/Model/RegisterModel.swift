4f56cbbd0215f47e0179601e1be530fe9c1a2b7e
Logiuratodanny <logiuratodanny@gmail.com>
2024-03-10 17:25:03 +0100

    -add model Class for Registration -Validating values in model Class

---
+++ CodingTask/Model/RegisterModel.swift
@@ -0,0 +1,44 @@
+//
+//  RegisterModel.swift
+//  CodingTask
+//
+//  Created by Danny Logiurato on 10.03.24.
+//
+
+import Foundation
+
+struct RegistrationModel {
+    var name: String
+    var email: String
+    var birthday: Date
+    
+    func isValid() -> Bool {
+        return validateName() && isValidEmail() && validateBirthday()
+    }
+    
+    private func validateName() -> Bool {
+        return !name.isEmpty
+    }
+    
+    func isValidEmail() -> Bool {
+        let regex = "^[^@]+@[^@.]+\\.[^@.]{2,}$"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", regex)
+        return emailPred.evaluate(with: email)
+    }
+    
+    private func validateBirthday() -> Bool {
+        let calendar = Calendar.current
+        var components = DateComponents()
+        components.year = 1900
+        components.month = 1
+        components.day = 1
+        guard let startDate = calendar.date(from: components) else { return false }
+        
+        components.year = 2022
+        components.month = 12
+        components.day = 31
+        guard let endDate = calendar.date(from: components) else { return false }
+        
+        return birthday >= startDate && birthday <= endDate
+    }
+}
